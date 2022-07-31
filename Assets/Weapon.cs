c5d048b39171198ab18e3af2561902f719662be3
hktrollgames <hktrollgames@gmail.com>
2022-07-31 18:14:32 +0900

    미사일 날아감, 몬스터 맞으면 파괴시킴

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -5,4 +5,26 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public Vector3 destination;
+    public float speed = 10;
+    private void Update()
+    {
+        Vector3 direction = destination - transform.position;
+        direction.Normalize();
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, destination) < 0.01f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Monster monster = collision.GetComponent<Monster>();
+        if (monster == null)
+            return;
+
+        Destroy(monster.gameObject);
+        Destroy(gameObject);
+    }
 }

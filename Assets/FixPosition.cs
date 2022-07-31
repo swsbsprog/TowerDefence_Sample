14c4883538cae0966bc92428b99e877c457698ca
hktrollgames <hktrollgames@gmail.com>
2022-07-31 18:31:47 +0900

    리젠 버튼 작동안하는 버그 수정

---
+++ Assets/FixPosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixPosition : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        transform.position = Vector3.zero;
+    }
+}

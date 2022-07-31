465565c4ba5414da64ea4e8391be7f0cb91df648
hktrollgames <hktrollgames@gmail.com>
2022-07-31 14:32:18 +0900

    스폰 포인트 설치

---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        print("메뉴 표시");
+    }
+}
